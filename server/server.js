fb1265bda31d6bce22b38d6cc07ced89c2bf9c38
Armando Tapia <armandotapia115@hotmail.com>
2019-10-17 16:31:15 -0500

    se agregó algunas funcionalidades de sockets

--- server/server.js
+++ server/server.js
@@ -1,23 +1,45 @@
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
-// Sockets
+const path = require("path");
+//=========================================
+// Configuracion de Sockets
 const socketIO = require("socket.io");
-
+const http = require("http");
+// ========================================
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, '../public');
+// ========================================
+// Configuracion de Sockets
+let server = http.createServer(app);
+// ========================================
+
+const publicPath = path.resolve(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+// ========================================
+// Configuracion de Sockets
+// Esta es la comunicacion del backend
+let io = socketIO(server);
+io.on("connection", client => {
+  console.log("usuario conectado");
+  client.on("disconnect", () => {
+    console.log("Usuario desconectado");
+  });
 
-
-
-app.listen(port, (err) => {
-
-    if (err) throw new Error(err);
-
-    console.log(`Servidor corriendo en puerto ${ port }`);
-
+  // Escuchar al cliente
+  client.on("enviarMensaje", data => {
+    console.log(data);
+  });
 });
+// ========================================
+
+// ========================================
+// Configuracion de Sockets
+server.listen(port, err => {
+  if (err) throw new Error(err);
+
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
+// ========================================
